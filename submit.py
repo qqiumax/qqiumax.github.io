c96a1f87d25bc61be1b36c2c922b45570b009946
qqiumax <maxqqqq@hotmail.com>
2022-07-05 13:57:14 +0800

    new blog
    Signed-off-by: qqiumax <maxqqqq@hotmail.com>

--- submit.py
+++ submit.py
@@ -9,9 +9,10 @@ url = sitemap_urls
 key = "387741bd4879437eb1fcc84d66b0b28e"
 location = 'https://qqiumax.github.io/387741bd4879437eb1fcc84d66b0b28e.txt'
 for i in url:
-          endpoint = f"https://bing.com/indexnow?url={i}&key={key}&keyLocation={location}"
-          response = requests.get(endpoint)
-          print(i)
-          print(endpoint)
-          print(response.status_code, response.content)
+    i == str(i)
+    endpoint = f"https://yandex.com/indexnow?url={i}&key={key}&keyLocation={location}"
+    response = requests.get(endpoint)
+    print(i)
+    print(endpoint)
+    print(response.status_code, response.content)
           
